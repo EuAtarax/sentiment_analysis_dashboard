3298104a1171d99ada73bbae65751d45b6edfd03
Niklas Khoss <ds21m011@technikum-wien.at>
2022-12-14 19:48:34 +0100

    create folder for model

--- download_model.py
+++ download_model.py
@@ -1,4 +1,8 @@
 from flair.models import TextClassifier
+import os
+
+if not os.path.isdir('models'):
+    os.mkdir('models')
 
 classifier = TextClassifier.load('en-sentiment')
 classifier.save('./models/sentiment-en-mix-distillbert_4.pt')
